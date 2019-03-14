efef6dc091683b86735e1506957213820a00c966
Ashish Arya <arya.ashish229@gmail.com>
2019-03-14 21:06:46 +0530

    C LANG MARCH14

---
+++ FIBONARC.BAK
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <stdio.h>
+void main()
+{
+//fibonarcy series
+int a=0,b=1,sum=0,n;
+printf ("SERIES");
+scanf ("%d",&n);
+printf("\n%d, %d",a,b);
+for (int i=2;i<=n;i++)
+{sum=a+b;
+ printf ("%d",sum);
+ a=b;
+ b=sum;
+
+}
+
+getch();
+
+
+}
---
+++ FIBONARC.C
@@ -0,0 +1,24 @@
+#include <conio.h>
+#include <stdio.h>
+void main()
+{
+
+//fibonarcy series
+int a=0,b=1,sum=0,n,i;
+clrscr();
+printf ("SERIES");
+scanf ("%d",&n);
+printf("\n%d, %d",a,b);
+for (i=3;i<=n;i++)
+{sum=a+b;
+ printf (", %d",sum);
+ a=b;
+ b=sum;
+
+}
+printf(". \n Ashish Arya  43513203118") ;
+
+getch();
+
+
+}
