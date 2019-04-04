3f28f2db12cf603dc152b2fd65476b58cb31cbb8
Ashish Arya <arya.ashish229@gmail.com>
2019-04-04 23:19:06 +0530

    ASIG-4APRIL

--- PATASG2.C
+++ PATASG2.C
@@ -2,16 +2,17 @@
 #include <conio.h>
 void main()
 {
-int n;
+int n,i,j;
 clrscr();
 printf("TERM");
 scanf ("%d",&n);
-for (int i=1;i<=n;i++)
+for (i=1;i<=n;i++)
 {  char ch='A';
-for(int j=1;j<=i;j++)
+for( j=1;j<=i;j++)
 {printf("%c",ch);
 ch++;}
 printf("\n");
 }
+printf("\n  Ashish Arya 43513203118");
 getch();
 }
